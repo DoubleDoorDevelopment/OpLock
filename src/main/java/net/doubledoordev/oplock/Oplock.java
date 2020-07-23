39eb655f8f95f7ff5392b1a88ec3bd1f6a1adbe2
Claycorp <Claycorp@doubledoordev.net>
2020-07-23 13:30:56 -0500

    Version bump...

--- src/main/java/net/doubledoordev/oplock/Oplock.java
+++ src/main/java/net/doubledoordev/oplock/Oplock.java
@@ -33,7 +33,7 @@ public class Oplock
 
     public static final String MOD_ID = "oplock";
     public static final String MOD_NAME = "OpLock";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
      */
