3b6f220747da9d99dda476942630ef9da1fdfbbf
Claycorp <Claycorp@doubledoordev.net>
2020-08-13 21:53:01 -0500

    Hopeful fix for potential NPE when kicking players in very specific cases I can't reproduce easily.

--- src/main/java/net/doubledoordev/oplock/Oplock.java
+++ src/main/java/net/doubledoordev/oplock/Oplock.java
@@ -33,7 +33,7 @@ public class Oplock
 
     public static final String MOD_ID = "oplock";
     public static final String MOD_NAME = "OpLock";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
      */
@@ -97,9 +97,9 @@ public class Oplock
             {
                 EntityPlayerMP player = playerList.getPlayerByUUID(uuid);
 
-                if (delayedKickQueue.get(uuid) <= currentTick)
+                if (delayedKickQueue.get(uuid) <= currentTick && !player.hasDisconnected())
                 {
-                    playerList.getPlayerByUUID(player.getUniqueID()).connection.disconnect(new TextComponentString(ModConfig.loginDisconnectMessage));
+                    player.connection.disconnect(new TextComponentString(ModConfig.loginDisconnectMessage));
                     delayedKickQueue.remove(uuid);
                 }
             }
